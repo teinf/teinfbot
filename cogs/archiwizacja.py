cefdfbba22488a4591b852d91ea73e516928724c
Kamil <panpouran@gmail.com>
2019-12-29 12:27:44 +0100

    poprawka archiwizacji

--- cogs/archiwizacja.py
+++ cogs/archiwizacja.py
@@ -10,29 +10,27 @@ class Logs(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         self.logChannelID = 660770250001874944
-        self.logsChannel = bot.get_channel(self.logChannelID)
+        self.logsChannel = self.bot.get_channel(self.logChannelID)
 
     @commands.Cog.listener()
     async def on_message(self, message):
         # wysyłanie wiadomosci do logów
-        if message.channel.id != logChannelID and message.author.id != bot.user.id:
+        if message.channel.id != self.self.logChannelID and message.author.id != self.bot.user.id:
             msgEm = discord.Embed(
                 title=f"{message.author}", description=f"Wiadomość: {message.content}", colour=discord.Color.green())
             msgEm.set_footer(
                 text=f'{message.channel}, ID: {message.author.id}')
-            logsChannel = bot.get_channel(logChannelID)
-            await logsChannel.send(embed=msgEm)
+            await self.logsChannel.send(embed=msgEm)
 
     @commands.Cog.listener()
     async def on_message_delete(self, message):
         # wysyłanie wiadomosci do logów
-        if message.channel.id != logChannelID and message.author.id != bot.user.id:
+        if message.channel.id != self.logChannelID and message.author.id != bot.user.id:
             msgEm = discord.Embed(
                 title=f"USUNIĘTO: {message.author}", description=f"Wiadomość: {message.content}", colour=discord.Color.red())
             msgEm.set_footer(
                 text=f'{message.channel}, ID: {message.author.id}')
-            logsChannel = bot.get_channel(logChannelID)
-            await logsChannel.send(embed=msgEm)
+            await self.logsChannel.send(embed=msgEm)
 
 
 def setup(bot):

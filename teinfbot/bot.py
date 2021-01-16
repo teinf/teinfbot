be6fc6dd8aa2848bac805622da3d5240efe0e110
Kamil Kozioł <koziol.kamil@protonmail.com>
2021-01-16 23:07:48 +0100

    Running tests doesnt require loading bot extensions

--- teinfbot/bot.py
+++ teinfbot/bot.py
@@ -13,15 +13,8 @@ class TeinfBot(commands.Bot):
         )
 
         self.guild_id = 406476256646004736
-
+        self.exts = extensions
         self.token = token
-
-        for extension in extensions:
-            try:
-                self.load_extension(extension)
-                print(f"[EXT] Success - {extension}")
-            except commands.ExtensionNotFound:
-                print(f"[EXT] Failed - {extension}")
 
     def run(self):
         try:
@@ -30,6 +23,12 @@ class TeinfBot(commands.Bot):
             self.loop.run_until_complete(self.bot_close())
 
     async def bot_start(self):
+        for extension in self.exts:
+            try:
+                self.load_extension(extension)
+                print(f"[EXT] Success - {extension}")
+            except commands.ExtensionNotFound:
+                print(f"[EXT] Failed - {extension}")
         await self.login(self.token)
         await self.connect()
 

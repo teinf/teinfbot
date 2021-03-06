2c32c01da9988e9427c6322639863bf64a82f2a4
Kamil Kozioł <koziol.kamil@protonmail.com>
2021-03-06 16:24:26 +0100

    Dodano kolejkę piosenek

--- teinfbot/commands/music.py
+++ teinfbot/commands/music.py
@@ -15,14 +15,31 @@ from typing import List
 
 
 class Music(commands.Cog):
+    
+    queue: List[MusicVideo] = []
+    FFMPEG_OPTS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+
     def __init__(self, bot: TeinfBot):
         self.bot: TeinfBot = bot
-        self.queue = List[MusicVideo]
 
     @cog_ext.cog_slash(name="music", description="Granie muzyki", guild_ids=guild_ids)
     async def __music(self, ctx: SlashContext):
         await ctx.ack(True)
+  
+    @classmethod
+    def clear_queue(cls):
+        cls.queue = []
 
+    @classmethod
+    def play_next_song(cls, voice: discord.VoiceClient):
+
+        if len(cls.queue) > 0:
+            cls.queue.pop(0)
+
+        if len(cls.queue) > 0:
+            voice.play(discord.FFmpegPCMAudio(cls.queue[0].source, **cls.FFMPEG_OPTS), after=lambda x: cls.play_next_song(voice))
+            
+        
     @cog_ext.cog_subcommand(base='music', name='play', guild_ids=guild_ids, options=[
         manage_commands.create_option(
             name="url",
@@ -38,8 +55,15 @@ class Music(commands.Cog):
             await ctx.send("Niepoprawny link lub zła strona")
             return
         
+        if len(self.queue) >= 1:
+            self.queue.append(music_video)
+            return
+        
+        elif len(self.queue) == 0:
+            self.queue.append(music_video)
 
-        voice = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
+        voice: discord.VoiceClient = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
+        
 
         channel = ctx.author.voice.channel
         if voice and voice.is_connected():
@@ -47,8 +71,7 @@ class Music(commands.Cog):
         else:
             voice = await channel.connect()
 
-        FFMPEG_OPTS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-        voice.play(discord.FFmpegPCMAudio(music_video.source, **FFMPEG_OPTS))
+        voice.play(discord.FFmpegPCMAudio(music_video.source, **self.FFMPEG_OPTS), after=lambda x: self.play_next_song(voice))
 
     @cog_ext.cog_subcommand(base='music', name='pause', guild_ids=guild_ids)
     async def __music_pause(self, ctx: SlashContext):
@@ -69,10 +92,21 @@ class Music(commands.Cog):
     @cog_ext.cog_subcommand(base='music', name='stop', guild_ids=guild_ids)
     async def __music_stop(self, ctx: SlashContext):
         await ctx.ack(True)
-        voice: discord.VoiceClient = discord.utils.get(
-            self.bot.voice_clients, guild=ctx.guild)
-        if voice.is_playing():
-            voice.stop()
+        voice: discord.VoiceClient = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
+        self.clear_queue()
+        voice.stop()
+
+    @cog_ext.cog_subcommand(base='music', name='skip', guild_ids=guild_ids)
+    async def __music_skip(self, ctx: SlashContext):
+        await ctx.ack(True)
+        voice: discord.VoiceClient = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
+        voice.stop()
+    
+    @cog_ext.cog_subcommand(base='music', name='clear', guild_ids=guild_ids)
+    async def __music_clear(self, ctx: SlashContext):
+        await ctx.ack(True)
+        voice: discord.VoiceClient = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
+        self.clear_queue()
 
 
 def setup(bot: TeinfBot):
